d15a15cca7e4925bb6653a2b6def097dd1c475ea
Danyl Brahin <inicerer141@gmail.com>
2023-09-09 22:04:23 +0300

    Fix proguard issue and absent apikey

--- app/src/main/java/com/dezdeqness/tmdb/data/service/MovieService.kt
+++ app/src/main/java/com/dezdeqness/tmdb/data/service/MovieService.kt
@@ -1,5 +1,6 @@
 package com.dezdeqness.tmdb.data.service
 
+import com.dezdeqness.tmdb.BuildConfig
 import com.dezdeqness.tmdb.data.model.remote.MoviePage
 import retrofit2.Call
 import retrofit2.http.GET
@@ -11,7 +12,8 @@ interface MovieService {
     fun getMoviePage(
         @Query(value = "page") page: Int,
         @Query(value = "sort_by") sortBy: String = "primary_release_date.desc",
-        @Query(value = "vote_average") voteAverage: Double = 7.0,
-        @Query(value = "vote_count") voteCount: Int = 100,
+        @Query(value = "vote_average.gte") voteAverage: Double = 7.0,
+        @Query(value = "vote_count.gte") voteCount: Int = 100,
+        @Query(value = "api_key") apiKey: String = BuildConfig.APIKEY,
     ): Call<MoviePage>
 }
