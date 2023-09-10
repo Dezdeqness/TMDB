5176c782c58d12a3bcccf3aac40ba8e0f12aa012
Danyl Brahin <inicerer141@gmail.com>
2023-09-10 18:19:45 +0300

    Fix state when tabs were recreated each time screen is opened

--- app/src/main/java/com/dezdeqness/tmdb/presentation/features/home/HomePage.kt
+++ app/src/main/java/com/dezdeqness/tmdb/presentation/features/home/HomePage.kt
@@ -1,7 +1,11 @@
 package com.dezdeqness.tmdb.presentation.features.home
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
@@ -9,6 +13,7 @@ import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -17,12 +22,16 @@ import com.dezdeqness.tmdb.R
 import com.dezdeqness.tmdb.presentation.features.favourite.FavouritePage
 import com.dezdeqness.tmdb.presentation.features.feed.FeedPage
 import com.dezdeqness.tmdb.ui.theme.TmdbTheme
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomePage(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = hiltViewModel(),
 ) {
+
+    val scope = rememberCoroutineScope()
 
     val state = viewModel.homeState.collectAsState()
 
@@ -37,40 +46,75 @@ fun HomePage(
             stringResource(id = R.string.tab_favourites_title),
         )
 
-        TabRow(
-            selectedTabIndex = tabIndex,
-            indicator = { tabPositions ->
-                val index = state.value.selectedTab
-                if (index < tabPositions.size) {
-                    TabRowDefaults.Indicator(
-                        height = 2.dp,
-                        color = TmdbTheme.colors.indicator,
-                        modifier = Modifier.tabIndicatorOffset(tabPositions[index])
-                    )
+        val pagerState = rememberPagerState(initialPage = tabIndex)
+
+        Tabs(
+            tabIndex = tabIndex,
+            tabs = tabs,
+            onClick = { index ->
+                viewModel.onTabClicked(index = index)
+                scope.launch {
+                    pagerState.animateScrollToPage(index)
                 }
             },
-            containerColor = TmdbTheme.colors.tabContainer,
-        ) {
-            tabs.forEachIndexed { index, title ->
-                Tab(
-                    text = {
-                        Text(
-                            text = title,
-                            style = TmdbTheme.typography.tabTitle,
-                        )
-                    },
-                    selected = tabIndex == index,
-                    onClick = {
-                        viewModel.onTabClicked(index = index)
-                    },
-                    selectedContentColor = TmdbTheme.colors.tabSelected,
-                    unselectedContentColor = TmdbTheme.colors.tabUnSelected,
+        )
+
+        TabsContent(tabs = tabs, pagerState = pagerState)
+    }
+}
+
+@Composable
+fun Tabs(
+    tabIndex: Int,
+    tabs: List<String>,
+    onClick: (Int) -> Unit,
+) {
+    TabRow(
+        selectedTabIndex = tabIndex,
+        indicator = { tabPositions ->
+            if (tabIndex < tabPositions.size) {
+                TabRowDefaults.Indicator(
+                    height = 2.dp,
+                    color = TmdbTheme.colors.indicator,
+                    modifier = Modifier.tabIndicatorOffset(tabPositions[tabIndex])
                 )
             }
+        },
+        containerColor = TmdbTheme.colors.tabContainer,
+    ) {
+        tabs.forEachIndexed { index, title ->
+            Tab(
+                text = {
+                    Text(
+                        text = title,
+                        style = TmdbTheme.typography.tabTitle,
+                    )
+                },
+                selected = tabIndex == index,
+                onClick = {
+                    onClick(index)
+                },
+                selectedContentColor = TmdbTheme.colors.tabSelected,
+                unselectedContentColor = TmdbTheme.colors.tabUnSelected,
+            )
         }
-        when (tabIndex) {
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun TabsContent(
+    tabs: List<String>,
+    pagerState: PagerState,
+) {
+    HorizontalPager(
+        pageCount = tabs.size,
+        state = pagerState
+    ) { index ->
+        when (index) {
             0 -> FeedPage()
             1 -> FavouritePage()
         }
+
     }
 }
--- app/src/main/java/com/dezdeqness/tmdb/presentation/features/shared/ui/MovieTile.kt
+++ app/src/main/java/com/dezdeqness/tmdb/presentation/features/shared/ui/MovieTile.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults.cardColors
 import androidx.compose.material3.CardDefaults.cardElevation
@@ -33,6 +34,7 @@ fun MovieTile(
     modifier: Modifier = Modifier,
 ) {
     Card(
+        shape = RoundedCornerShape(4.dp),
         elevation = cardElevation(3.dp),
         colors = cardColors(
             containerColor = TmdbTheme.colors.backgroundLight,
